979727ebb72278b748665d864c6a419009247689
knyliu <131148428+knyliu@users.noreply.github.com>
2023-12-06 01:27:01 +0800

    Add files via upload

--- client/src/Create.js
+++ client/src/Create.js
@@ -32,7 +32,7 @@ export default function Create() {
 
   return (
     <div className="App">
-      <h1>Create A New Customer</h1>
+      <h1>Create</h1>
 
       <input
         placeholder="name"
@@ -40,17 +40,14 @@ export default function Create() {
         name="name"
         value={user.name}
       />
-      <br></br>
       <input
         placeholder="last name"
         onChange={handleChange}
         name="lastName"
         value={user.lastName}
       />
-      <br></br>
 
-      <button onClick={saveData}>Save</button>
-      <br></br>
+      <button onClick={saveData}>Save to MongoDB</button>
       <button onClick={() => navigate(-1)}>Back</button>
     </div>
   );
